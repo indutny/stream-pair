4b253440a82f76a05757330ff7bbe37fa9d69e04
Fedor Indutny <fedor@indutny.com>
2015-06-19 11:00:40 -0400

    initial

---
+++ test/api-test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+var streamPair = require('../');
+
+describe('StreamPair', function() {
+  var pair = null;
+
+  beforeEach(function() {
+    pair = streamPair.create();
+  });
+
+  it('should send data to other side', function(done) {
+    pair.write('hello');
+    assert.equal(pair.other.read().toString(), 'hello');
+
+    pair.other.on('end', done);
+    pair.end(function() {
+      pair.other.read();
+    });
+  });
+});
